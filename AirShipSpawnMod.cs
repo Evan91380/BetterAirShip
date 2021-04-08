9a9e7584aac7f2c988c970e0b9291533aa397375
Evan91380 <evanpetit5@gmail.com>
2021-04-08 16:39:42 +0200

    Random -> Synchronized

--- AirShipSpawnMod.cs
+++ AirShipSpawnMod.cs
@@ -16,7 +16,7 @@ namespace AirShipSpawn{
         public Harmony Harmony { get; } = new Harmony(Id);
 
         public static CustomOptionHeader AirshipHeader = CustomOptionHeader.AddHeader("\n[2EADFFFF]BetterAirShip Options :[]", false);
-        public static CustomStringOption TypeSpawn = CustomStringOption.AddString("Type Of Spawn", new string[] { "Normal", "Fixed", "Random" });
+        public static CustomStringOption TypeSpawn = CustomStringOption.AddString("Type Of Spawn", new string[] { "Normal", "Fixed", "Synchronized" });
         public static CustomToggleOption NewSpawn = CustomToggleOption.AddToggle("Add new spawn", false);
         public static CustomToggleOption MeetingRespawn = CustomToggleOption.AddToggle("Choose spawn after meeting", true);
         public static CustomNumberOption minTimeDoor = CustomNumberOption.AddNumber("Min time for door swipe", 0.4f, 0f, 1f, 0.05f);
