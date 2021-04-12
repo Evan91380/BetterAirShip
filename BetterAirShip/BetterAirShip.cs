4ba7b83a6092b5bb1c19454a22754deba2e622c3
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2021-04-12 16:15:42 +0200

    Fade

--- BetterAirShip/BetterAirShip.cs
+++ BetterAirShip/BetterAirShip.cs
@@ -1,7 +1,6 @@
 ﻿using BepInEx;
 using BepInEx.IL2CPP;
 using BepInEx.Logging;
-using BetterAirShip.Utility;
 using Essentials.Options;
 using HarmonyLib;
 using Reactor;
@@ -18,11 +17,11 @@ namespace BetterAirShip {
         public Harmony Harmony { get; } = new Harmony(Id);
 
         public static CustomOptionHeader AirshipHeader = CustomOptionHeader.AddHeader("[0015CCFF]BetterAirShip General :[]");
-        public static CustomStringOption TypeSpawn = CustomStringOption.AddString("Type Of Spawn", new string[] { "Normal", "Fixed", "Random Synchronized" });
+        public static CustomStringOption TypeSpawn = CustomStringOption.AddString("Spawn type", new string[] { "Normal", "Fixed", "Random Synchronized" });
         public static CustomToggleOption NewSpawn = CustomToggleOption.AddToggle("Add new spawn", false);
         public static CustomToggleOption MeetingRespawn = CustomToggleOption.AddToggle("Meeting Respawn", false);
         public static CustomToggleOption CallPlateform = CustomToggleOption.AddToggle("Call Plateform Button", false);
-        public static CustomToggleOption Teleportation = CustomToggleOption.AddToggle("Teleportation Meeting/Security", false);
+        public static CustomToggleOption Teleportation = CustomToggleOption.AddToggle("Meeting/Security teleportation", false);
         public static CustomNumberOption minTimeDoor = CustomNumberOption.AddNumber("Min time for door swipe", 0.4f, 0f, 10f, 0.05f);
 
         public static CustomOptionHeader AirshipTasksHeader = CustomOptionHeader.AddHeader("[0015CCFF]BetterAirShip Tasks :[]");
--- BetterAirShip/Patch/TeleportationMeeting.cs
+++ BetterAirShip/Patch/TeleportationMeeting.cs
@@ -16,7 +16,7 @@ namespace BetterAirShip.Patch {
         public static void Prefix(PlayerControl __instance) {
             if (BetterAirShip.Teleportation.GetValue()) {
                 if (!TeleportationStarted && Vector2.Distance(__instance.transform.position, new Vector2(17.331f, 15.236f)) < 0.5f && UnityEngine.Object.FindObjectOfType<AirshipStatus>() != null)
-                Coroutines.Start(CoTeleportPlayer(__instance));
+                    Coroutines.Start(CoTeleportPlayer(__instance));
             }
         }
 
@@ -48,13 +48,13 @@ namespace BetterAirShip.Patch {
             DestroyableSingleton<HudManager>.Instance.FullScreen.enabled = true;
 
             if (fadeAway) {
-                for (float i = 1; i >= 0; i -= 1 / Time.deltaTime) {
+                for (float i = 1; i >= 0; i -= Time.deltaTime) {
                     DestroyableSingleton<HudManager>.Instance.FullScreen.color = new Color(0, 0, 0, i);
                     yield return null;
                 }
             }
             else {
-                for (float i = 0; i <= 1; i += 1 / Time.deltaTime) {
+                for (float i = 0; i <= 1; i += Time.deltaTime) {
                     DestroyableSingleton<HudManager>.Instance.FullScreen.color = new Color(0, 0, 0, i);
                     yield return null;
                 }
@@ -68,6 +68,7 @@ namespace BetterAirShip.Patch {
             TeleportationStarted = true;
             yield return Fade(false, false);
             instance.NetTransform.RpcSnapTo(new Vector2(5.753f, -10.011f));
+            yield return new WaitForSeconds(0.3f);
             yield return Fade(true, true);
             TeleportationStarted = false;
 
